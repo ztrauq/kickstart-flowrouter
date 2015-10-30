1042187390df96f4a5bddb31d56b490826c358e1
garrilla <garry@garrilla.com>
2015-10-30 19:31:31 +0000

    post holiday

---
+++ modules/BlogApp/client/components/blogutils/SideBar/ListTags.jsx
@@ -0,0 +1,86 @@
+/*
+todo -
+  add functionality to search
+  - currently only single pattern so could add
+  -- keyword
+  -- phrase
+  -- etc
+ */
+
+import { Component, PropTypes } from 'react';
+import ReactMixin from 'react-mixin';
+import ReactIntl from 'react-intl'
+import { Blog } from 'BlogApp/collections/Blog';
+import 'BlogApp/blog-methods.js';
+
+@ReactMixin.decorate(ReactMeteorData)
+export default class ListSearch extends Component {
+
+  constructor() {
+    super();
+    //this.state = {value: ''};
+  }
+
+  getMeteorData () {
+    Meteor.subscribe('blog',Session.get('blogSubFilter'));
+    let query = {quicklist: true};
+    var blog = Blog.find(query).fetch();
+    return {
+      blog
+    };
+  }
+
+  search () {
+    const value = event.target.value;
+    this.setState({value: value})
+    const filter = value.length > 0 ? {type: 'search', str: value} : null;
+    Session.set('blogSubFilter', filter);
+  }
+
+  clearSearch () {
+    //this.setState({value: ''})
+    Session.set('blogSubFilter', null);
+  }
+
+  render () {
+    const filter = Session.get('blogSubFilter');
+    const value = filter ? filter.str : '';
+    const items = this.data.blog.map(function(item, i){
+      return <QuickListItems data={item} key={i}/>
+    });
+    return (
+      <div>
+        <h4 className="ui top attached white inverted header">
+          <i className="search icon" />
+          <div className="content">
+            Search
+          </div>
+        </h4>
+        <div className="ui attached basic segment">
+          <p>A simple, case-insensitive pattern matching search, finds a 'word' or 'phrase string' in the title or summary.</p>
+        </div>
+        <div className="ui attached segment">
+          <div className="ui fluid action input">
+            <input value={value} id="blogSearch" type="text" placeholder="Search..."
+              onChange={this.search}/>
+            <button className="ui icon button" onClick={this.clearSearch}>
+              <i className="remove icon" />
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
+
+const QuickListItems = React.createClass({
+  render: function () {
+    const path = "/post/" + this.props.data.slug ;
+    return (
+      <a className="item" href={path}>
+        <div className="header">{this.props.data.title}</div>
+        <div className="description">{this.props.data.summary}</div>
+      </a>
+    )
+  }
+});
